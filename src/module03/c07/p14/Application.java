b57c0679663cbf318236712661eef21458ff04dd
Chris <chris.hollander@gmail.com>
2019-09-09 23:59:48 -0400

    Added solution for 03.07.14

--- src/module03/c07/p14/Application.java
+++ src/module03/c07/p14/Application.java
@@ -1,5 +1,7 @@
 package module03.c07.p14;
 
+import java.util.Scanner;
+
 public class Application {
 	/*
 	 * (Airline Reservations System) A small airline has just purchased a computer
@@ -25,13 +27,86 @@ public class Application {
 	 * When the economy section is full, your application should ask the person if
 	 * it’s acceptable to be placed in the first-class section (and vice versa). If
 	 * yes, make the appropriate seat assignment. If no, display the message
-	 * 
 	 * "Next flight leaves in 3 hours."
 	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	    final int CAPACITY = 10;
+	    final int FIRST_CAPACITY = 5;
+	    
+	    Scanner in = new Scanner(System.in);
+	    boolean[] seats = new boolean[CAPACITY];
+	    
+	    boolean done = false;
+	    int currentFirstSeat = 0;
+	    int currentEconSeat = FIRST_CAPACITY;
+	    boolean firstFull = false;
+	    boolean econFull = false;
+	    
+	    while(!done) {
+    	    System.out.print("Please type 1 for First Class and Please type 2 for Economy (or 3 to quit): ");
+    	    int choice = in.nextInt();
+	    
+    	    switch(choice) {
+    	    case 1:
+    	        if(currentFirstSeat < FIRST_CAPACITY) {
+    	            seats[currentFirstSeat] = true;
+    	            currentFirstSeat++;
+    	        } else {
+    	            firstFull = true;
+    	        }
+    	        break;
+    	    case 2:
+    	        if(currentEconSeat < CAPACITY) {
+                    seats[currentEconSeat] = true;
+                    currentEconSeat++;
+                } else {
+                    econFull = true;
+                }
+    	        break;
+    	    default:
+    	        // is this really a good idea? is the user expecting to quit if they enter 4?
+    	        done = true;
+    	        
+    	        // what if we wanted to break the loop to quit instead of setting a flag? could we still use a switch statement?
+    	    }
+    	    
+    	    if(!done) {
+    	        if(firstFull && !econFull) {
+    	            System.out.print("First class is full, please type 1 if you wish to be seated in economy (or 3 to return): ");
+    	            choice = in.nextInt();
+    	            if(choice == 1) {
+    	                if(currentEconSeat < CAPACITY) {
+    	                    seats[currentEconSeat] = true;
+    	                    currentEconSeat++;
+    	                } else {
+    	                    econFull = true;
+    	                }
+    	            } else {
+    	                continue;
+    	            }
+    	        }
+    	        if(!firstFull && econFull) {
+    	            System.out.print("Economy class is full, please type 1 if you wish to be seated in first class (or 3 to return): ");
+                    choice = in.nextInt();
+                    if(choice == 1) {
+                        if(currentFirstSeat < CAPACITY) {
+                            seats[currentFirstSeat] = true;
+                            currentFirstSeat++;
+                        } else {
+                            firstFull = true;
+                        }
+                    } else {
+                        continue;
+                    }
+    	        }
+    	        if(firstFull && econFull) {
+                    System.out.println("I'm sorry, but the flight is full. The next flight leaves in 3 hours.");
+                    done = true;
+    	        }
+    	    }
+	    }
 	}
-
+	
+	// Notice that this code is very ugly. Methods would make a big step in making it more readable.
 }
